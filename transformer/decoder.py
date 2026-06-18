bfba2c821897caf23c8a99405321b227e02ed0d5
sunnycloud <191893999+sunnycloudhust@users.noreply.github.com>
2026-06-18 14:52:11 +0700

    add decoder

---
+++ transformer/decoder.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from self_attention import SelfAttention
+
+class Decoder(nn.Module):
+    def __init__(self, self_attention, d_model, d_ff, num_head, 
+                bias=True, dropout=0.1):
+        super().__init__()
+        self.self_attention = self_attention
+        self.d_model = d_model
+        self.dropout = dropout
+        self.bias = bias
+        self.d_ff = d_ff
+        self.num_head = num_head
+        
+    def forward(self, x, mask, )
+        
--- transformer/encoder.py
+++ transformer/encoder.py
@@ -6,13 +6,12 @@ from self_attention import SelfAttention
 from cross_attention import CrossAttention
 
 class Encoder(nn.Module):
-    def __init__(self, self_attention, mask, cross_attention, d_model, d_ff,
+    def __init__(self, self_attention, cross_attention, d_model, d_ff,
                 num_head, bias=True, dropout=0.1):
         super().__init__()
         self.d_model = d_model
         self.num_head = num_head
         self.dropout = dropout
-        self.mask = mask
         self.bias = bias
         self.d_ff = d_ff
         self.self_attention = self_attention
@@ -33,12 +32,12 @@ class Encoder(nn.Module):
         return output
     
 if __name__ == '__main__':
-    vocab_size = 100
-    d_model = 240
+    vocab_size = 100000
+    d_model = 512
     seq_len = 20
-    d_ff = 480 
-    num_head = 10
-    d_embed = 300
+    d_ff = 1024
+    num_head = 8
+    d_embed = 256
     bias = True
     batch_size = 256
     mask = None
@@ -48,7 +47,7 @@ if __name__ == '__main__':
     v_state = torch.randint(0, vocab_size, (batch_size, seq_len, d_model)).float()  
     self_attention = SelfAttention(d_model, num_head, bias, dropout)
     cross_attention = CrossAttention(d_model, num_head, bias, dropout)
-    encoder = Encoder(self_attention, mask, cross_attention, d_model, d_ff, num_head, bias, dropout)
+    encoder = Encoder(self_attention, cross_attention, d_model, d_ff, num_head, bias, dropout)
     output = encoder(x, k_state, v_state, mask=None)
     print(output.shape)
     
