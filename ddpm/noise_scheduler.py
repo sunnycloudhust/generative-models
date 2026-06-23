cc67a2cea46b168d709ddba8a5e0e7b8fa8f38a3
sunnycloud <191893999+sunnycloudhust@users.noreply.github.com>
2026-06-23 15:34:02 +0700

    Complete DDPM noise scheduler add_noise

--- ddpm/noise_scheduler.py
+++ ddpm/noise_scheduler.py
@@ -14,11 +14,18 @@ class LinearNoiseScheduler:
         self.sqrt_one_minus_alpha_cum_prod = torch.sqrt(1.0 - self.alpha_cum_prod)
     
     def add_noise(self, original, noise, t):
-        
         original_shape = original.shape
         batch_size = original.shape[0]
-        sqrt_alpha_cum_prod = self.sqrt_alpha_cum_prod[t].reshape(batch_size)
-    
+
+        sqrt_alpha_cum_prod = self.sqrt_alpha_cum_prod[t].reshape(batch_size)  # shape (batch_size,)
+        sqrt_one_minus_alpha_cum_prod = self.sqrt_one_minus_alpha_cum_prod[t].reshape(batch_size)  # shape (batch_size,)
+
+        for _ in range(len(original_shape) - 1):
+            sqrt_alpha_cum_prod = sqrt_alpha_cum_prod.unsqueeze(-1)
+            sqrt_one_minus_alpha_cum_prod = sqrt_one_minus_alpha_cum_prod.unsqueeze(-1)
+
+        return sqrt_alpha_cum_prod * original + sqrt_one_minus_alpha_cum_prod * noise
+
 
 if __name__ == "__main__":
     scheduler = LinearNoiseScheduler(
