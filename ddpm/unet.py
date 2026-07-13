866b1ca17932a22126a9629a732d4f80f33dd9cf
sunnycloud <191893999+sunnycloudhust@users.noreply.github.com>
2026-07-13 22:01:50 +0700

    Implement down block forward pass

--- ddpm/unet.py
+++ ddpm/unet.py
@@ -20,11 +20,13 @@ class DownBlock(nn.Module):
     def __init__(self, in_channels, out_channels, t_emb_dim, down_sample, num_heads):
         super().__init__()
         self.down_sample = down_sample
+        # Expected output shape = (batch_size, in_channels, height, width)
         self.resnet_conv_first = nn.Sequential(
             nn.GroupNorm(8, in_channels),
             nn.SiLU(),
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
         )
+        # mapping the time embedding to the same dim as out_channels
         self.t_emb_layers = nn.Sequential(
             nn.SiLU(),
             nn.Linear(t_emb_dim, out_channels)
@@ -33,13 +35,30 @@ class DownBlock(nn.Module):
             nn.GroupNorm(8, out_channels),
             nn.SiLU(),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        )
+        ) # Expected output shape = (batch_size, out_channels, height, width)
+        
         self.attention_norm = nn.GroupNorm(8, out_channels)
         self.attention = nn.MultiheadAttention(out_channels, num_heads, batch_first=True)
         self.residual_input_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
         self.down_sample_conv = nn.Conv2d(out_channels, out_channels, kernel_size=4,
                                           stride=2, padding=1) if self.down_sample else nn.Identity()
 
+    def forward(self, x, t_emb):
+        out = self.resnet_conv_first(x)
+        out = out + self.t_emb_layers(t_emb)[:, :, None, None]
+        out = self.resnet_conv_second(out)
+        out = out + self.residual_input_conv(x)
+
+        batch_size, channels, height, width = out.shape
+        attn_input = self.attention_norm(out)
+        attn_input = attn_input.reshape(batch_size, channels, height * width)
+        attn_input = attn_input.transpose(1, 2)
+
+        attn_output, _ = self.attention(attn_input, attn_input, attn_input)
+        attn_output = attn_output.transpose(1, 2).reshape(batch_size, channels, height, width)
+        out = out + attn_output
+
+        return self.down_sample_conv(out)
 
 
 
