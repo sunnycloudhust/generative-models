e97ef8fae4c4ae6fd77a366763741dd554870a0e
sunnycloud <191893999+sunnycloudhust@users.noreply.github.com>
2026-07-16 08:15:38 +0700

    add midblock

--- ddpm/unet.py
+++ ddpm/unet.py
@@ -100,5 +100,36 @@ class MidBlock(nn.Module):
                 nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
             )
         ])
-        self.attention_norm = nn.GroupNorm(8, out_channels)
+        self.attention_norm = nn.GroupNorm(num_groups=8, num_channels=out_channels)
+        self.attention = nn.MultiheadAttention(out_channels, num_heads, batch_first=True)
         
+        self.residual_input_conv = nn.ModuleList([
+            nn.Conv2d(in_channels, out_channels, kernel_size=1),
+            nn.Conv2d(out_channels, out_channels, kernel_size=1)
+        ])
+    def forward(self, x, t_emb):
+        out = x
+        # --- 1. FIRST RESNET BLOCK ---
+        resnet_input = out
+        out = self.resnet_conv_first[0](out)
+        out = out + self.t_emb_layers[0](t_emb)[:, :, None, None]
+        out = self.resnet_conv_second[0](out)
+        out = out + self.residual_input_conv[0](resnet_input)
+        
+        # --- 2. ATTENTION BLOCK ---
+        batch_size, channels, h, w = out.shape
+        in_attn = out.reshape(batch_size, channels, h * w)
+        in_attn = self.attention_norm(in_attn)
+        in_attn = in_attn.transpose(1, 2)
+        out_attn, _ = self.attention(in_attn, in_attn, in_attn)
+        out_attn = out_attn.transpose(1, 2).reshape(batch_size, channels, h, w)
+        out = out + out_attn
+        
+        # --- 3. SECOND RESNET BLOCK ---
+        resnet_input = out
+        out = self.resnet_conv_first[1](out)
+        out = out + self.t_emb_layers[1](t_emb)[:, :, None, None]
+        out = self.resnet_conv_second[1](out)
+        out = out + self.residual_input_conv[1](resnet_input)
+        
+        return out
