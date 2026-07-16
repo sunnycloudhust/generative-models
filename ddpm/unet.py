b6dca00ff5da675d9aee5634ae4ac02d2c8ab48e
sunnycloud <191893999+sunnycloudhust@users.noreply.github.com>
2026-07-16 08:17:21 +0700

    add upblock

--- ddpm/unet.py
+++ ddpm/unet.py
@@ -133,3 +133,54 @@ class MidBlock(nn.Module):
         out = out + self.residual_input_conv[1](resnet_input)
         
         return out
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, t_emb_dim, up_sample, num_heads):
+        super().__init__()
+        self.up_sample = up_sample
+        
+        self.resnet_conv_first = nn.Sequential(
+            nn.GroupNorm(num_groups=8, num_channels=in_channels),
+            nn.SiLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        )
+        
+        self.t_emb_layers = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(t_emb_dim, out_channels)
+        )
+        
+        self.resnet_conv_second = nn.Sequential(
+            nn.GroupNorm(num_groups=8, num_channels=out_channels),
+            nn.SiLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        )
+        
+        self.attention_norm = nn.GroupNorm(num_groups=8, num_channels=out_channels)
+        self.attention = nn.MultiheadAttention(out_channels, num_heads, batch_first=True)
+        self.residual_input_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.up_sample_conv = nn.ConvTranspose2d(
+            in_channels // 2, in_channels // 2, kernel_size=4, stride=2, padding=1
+        ) if self.up_sample else nn.Identity()
+    def forward(self, x, out_down, t_emb):
+        x = self.up_sample_conv(x)
+        x = torch.cat([x, out_down], dim=1)
+        
+        # Resnet block
+        out = x
+        resnet_input = out
+        out = self.resnet_conv_first(out)
+        out = out + self.t_emb_layers(t_emb)[:, :, None, None]
+        out = self.resnet_conv_second(out)
+        out = out + self.residual_input_conv(resnet_input)
+        
+        # Attention Block
+        batch_size, channels, h, w = out.shape
+        in_attn = out.reshape(batch_size, channels, h * w)
+        in_attn = self.attention_norm(in_attn)
+        in_attn = in_attn.transpose(1, 2)
+        out_attn, _ = self.attention(in_attn, in_attn, in_attn)
+        out_attn = out_attn.transpose(1, 2).reshape(batch_size, channels, h, w)
+        out = out + out_attn
+        
+        return out
+
